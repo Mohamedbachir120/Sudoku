08f03d582b63accf4be2dad77c7d89577815c7d4
moh <mohamedbhadjadji@gmail.com>
2020-10-06 00:04:30 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/*window.onload  = function(){
+    var http=new XMLHttpRequest();
+    http.open("GET","score.json",false);
+    http.send();
+    var v=JSON.parse(http.response);
+    console.log(v['score']);
+}
+*/
